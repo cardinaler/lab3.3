4baa1bfd8e4efa285c03310e86039780659d5a8c
cardinaler <118291025+cardinaler@users.noreply.github.com>
2023-12-23 11:30:37 +0300

    cardinaler
    yes

---
+++ lab3/lab3/Main.cs
@@ -0,0 +1,62 @@
+using System.IO.Compression;
+using System.Runtime.InteropServices.Marshalling;
+using System.Xml.Linq;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    public static DataItem Func1_FDI(double x)
+    {
+        return new DataItem(x, x - 2, x + 2);
+    }
+
+    public static void Func1_FV(double x, ref double y1, ref double y2)
+    {
+        y1 = x * x * x;
+        y2 = x * x * x;
+    }
+
+    public static double f1(double x) => x * x + 3 * x + 1; //Функция для начальной аппроксимации
+    public static void test1()
+    {
+        FValues F = Func1_FV;
+        double[] x = { 0, 0.1, 0.14, 0.2234, 0.311, 0.4999, 0.55, 0.6, 0.8, 1}; //Неравномерная сетка
+        
+        V2DataArray V2A = new V2DataArray("Quant", new DateTime(2023, 1, 1), x, F);
+ 
+        SplineData SP = new SplineData(V2A, 10, 1000);
+        SP.SplineMklCall(f1);
+        Console.WriteLine(SP.ToLongString("f7"));
+        SP.Save("Result_test1.txt", "f7");
+    }
+
+    public static void Func2_FV(double x, ref double y1, ref double y2)
+    {
+        y1 = x * x;
+        y2 = x * x * x;
+    }
+
+    public static double f2(double x) => x + 1; //Начальное приближение
+    public static void test2()
+    {
+        FValues F = Func2_FV;
+        double[] x = { 0, 0.0123, 0.144, 0.2234, 0.576, 0.689, 0.7, 0.8, 1}; //Неравномерная сетка
+
+        V2DataArray V2A = new V2DataArray("Quant", new DateTime(2023, 1, 1), x, F);
+
+        SplineData SP = new SplineData(V2A, 9, 1000);
+        SP.SplineMklCall(f2);
+        Console.WriteLine(SP.ToLongString("f7"));
+        SP.Save("Result_test2.txt", "f7");
+    }
+
+    static void Main()
+    {
+        FValues F1 = Func1_FV;
+        FDI F2 = Func1_FDI;
+        test1(); // Построение сплайна для функции x^3
+        test2(); // Построение сплайна для функции x^2
+    }
+}
+
+
---
+++ lab3/lab3/SplineData.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SplineData
+{
+    public V2DataArray? V2DataLink = null;
+    public int m;                  // Число узлов равномерной сетки == m (для построения)
+    public double[]? SplineValues = null; //Значения на сетке (для вычисления)
+    public int MaxItersNum;
+    public int StopReason;
+    public double ResidualMin;
+    public List<SplineDataItem>? ApproximationRes { get; set; }
+    public int ItersNum;
+    public SplineData(V2DataArray V2A, int NodesNum, int MaxItersNum)
+    {
+        this.V2DataLink = V2A;
+        this.m = NodesNum;
+        this.MaxItersNum = MaxItersNum;
+        this.SplineValues = new double[V2A.Net.Length];
+        ApproximationRes = new List<SplineDataItem>();
+    }
+
+    public void SplineMklCall(Func<double, double> F_Init) 
+    {
+        
+        // F_Init - функция, чтобы получить начальное приближение на равномерной сетке
+        
+        int nS = V2DataLink.Net.Length;                             //Число неравномерной узлов сетки
+        double[] grid = this.V2DataLink.Net;                        //Неравномерная сетка
+
+        double xL = this.V2DataLink.Net[0];
+        double xR = this.V2DataLink.Net[V2DataLink.Net.Length - 1];
+        double[] X = { xL, xR };                                    //Концы отрезка для равномерной сетки
+        double[] Uniform_grid = new double[m];                      //Равномерная сетка
+        double hS = (xR - xL) / (m - 1);                            // шаг сетки
+        Uniform_grid[0] = xL;
+        for (int j = 0; j < m; ++j)
+        {
+            Uniform_grid[j] = Uniform_grid[0] + hS * j;
+        }
+        double[] ApprStartVals = new double[m];
+        for (int i = 0; i < m; ++i)
+        {
+            ApprStartVals[i] = F_Init(Uniform_grid[i]); //Начальное приближение
+        }
+
+        int nY = 1;                                     //Размерность векторной функции
+        double[] y_true = new double[nS];
+        for (int i = 0; i < nS; ++i)
+        {
+            y_true[i] = this.V2DataLink.Field_values[0, i]; //Истинные значения на неравномерной сетке
+        }
+       
+        SplineValues = new double[nS];
+        try
+        {
+            OptimSplineInterpolation(
+                m,
+                X,
+                nY,
+                nS,
+                grid,
+                y_true,
+                ApprStartVals,
+                SplineValues,
+                ref StopReason,
+                ref ResidualMin,
+                MaxItersNum,
+                ref ItersNum);
+            ResidualMin = 0;
+            for(int i = 0; i < nS; ++i)
+            {
+                ResidualMin += (y_true[i] - SplineValues[i]) * (y_true[i] - SplineValues[i]);
+                ApproximationRes.Add(new SplineDataItem(grid[i], y_true[i], SplineValues[i]));
+            }
+            ResidualMin = Math.Sqrt(ResidualMin);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка сплайн-интерполяции\n{ex}");
+        }
+    }
+
+    [DllImport("Dll_for_lab3.dll",
+    CallingConvention = CallingConvention.Cdecl)]
+    public static extern
+    void OptimSplineInterpolation(
+    int m,                   // число узлов сплайна на равномерной сетке == m (для построения)
+    double[] X,              // массив узлов сплайна на равномерной сетке (для построения)
+    int nY,                  // размерность векторной функции (для построения)
+    int nS,                  // число узлов сетки, на которой вычисляются значения сплайна (для расчета)
+    double[] grid,           // Сетка, на которой происходит вычисление значений сплайна (для расчета)
+    double[] true_y,         // Истинные значения функции на сетке grid 
+    double[] ApprStartVals,  // Начальное приближение
+    double[] SplineValues,   // Значения сплайна на сетке grid (искомое)
+    ref int StpReason,       // Причина остановки
+    ref double MinResVal,    // Минимальное значение невязки
+    int MaxIters,            // Максимальное число итераций
+    ref int Iters);          // Сделаное число итераций
+
+
+    public string ToLongString(string format)
+    {
+        string ans = "";
+        ans += V2DataLink.ToLongString(format);
+        for(int i = 0; i < ApproximationRes.Count(); ++i)
+        {
+            ans += ApproximationRes[i].ToString(format);
+        }
+        ans += $"ResidualMin = {ResidualMin.ToString()}\n";
+        ans += $"StopReason = {StopReason.ToString()}\n";
+        ans += $"ItersNum = {ItersNum}\n";
+
+        return ans;
+    }
+
+    public void Save(string filename, string format)
+    {
+        try
+        {
+            StreamWriter writer = new StreamWriter(filename, false);
+            writer.Write(ToLongString(format));
+            writer.Close(); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception!");
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+
+}
+
